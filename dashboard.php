3ce18a579167488d7ed6b7c51759b8094648104d
VinTristanSollesta <vintristan.sollesta@gmail.com>
2026-02-06 10:59:40 +0800

    + tailwindcss installation + ui adjustment for dashboard

--- dashboard.php
+++ dashboard.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Ateneo de Iloilo HelpDesk</title>
-        <link rel="stylesheet" href="src/output.css">
+        <link rel="stylesheet" href="dist/styles.css">
         <meta name="robots" content="noindex">
 </head>
 <body class="bg-gray-100 text-gray-800">
